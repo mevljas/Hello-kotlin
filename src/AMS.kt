8cc14c10b08f3017d73ad58d96e68a021f07e579
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-02-27 21:01:27 +0100

    Added canAddFish function

--- src/AMS.kt
+++ src/AMS.kt
@@ -6,10 +6,40 @@ fun main(args: Array<String>) {
 
 }
 
+
+//put arguments without defaults first
+fun  shouldChangeWater (
+    day: String,
+    temperature: Int = 22,
+    dirty: Int = 20) : Boolean{
+    return true
+
+}
+
+fun  shouldChangeWater2 (
+    temperature: Int = 22,
+    dirty: Int = 20,
+    day: String){
+
+}
+
+
+
 fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
     println("Today is $day and the fish eat $food")
+
+    swim( 50, speed = "slow")
+
+    shouldChangeWater(day, 20, 50)
+    shouldChangeWater(day)
+    shouldChangeWater(day, dirty = 50)
+    shouldChangeWater2(day = "Monday")
+
+    if (shouldChangeWater(day)){
+        println("change the water today")
+    }
 }
 
 
@@ -32,3 +62,9 @@ fun fishFood(day : String) : String {
         else -> "fasting"
     }
 }
+
+fun swim(time: Int, speed: String = "fast"){
+    println("Swimming $speed")
+}
+
+
