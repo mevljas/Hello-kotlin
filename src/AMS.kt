1d17b58e6cca9e828e049b87665c2e8e546ad09e
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-02-27 21:21:14 +0100

    Added dirty sensor reading

--- src/AMS.kt
+++ src/AMS.kt
@@ -4,17 +4,59 @@ fun main(args: Array<String>) {
     println("Hello, ${args[0]}")
     feedTheFish()
 
+    var bubbles = 0
+    while (bubbles < 30){
+        bubbles ++
+    }
+
+    // you can't assign loops to a variable.
+
+    //repeat (function from a standard Library)
+    repeat(2) {
+        println("A fish is swimming")
+    }
+
 }
+
+fun getDirtySensorReading() = 20
 
 
 //put arguments without defaults first
 fun  shouldChangeWater (
     day: String,
     temperature: Int = 22,
-    dirty: Int = 20) : Boolean{
-    return true
+    //we can use function's return value as a default value.
+    //Dont use expensive functions. They are called at runTime.
+    dirty: Int = getDirtySensorReading()) : Boolean{
+
+    val isTooHot = temperature > 30
+    val isDirty = dirty > 30
+    val isSunday  = day == "sunday"
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+
+        else -> false
+    }
 
 }
+
+fun makeNewAquarium() = println("Builidng a new aquariium.....")
+
+//example of a expensive function.
+//any can hold any type of a object.
+fun aquariumStatusReport( aquarium: Any = makeNewAquarium()){
+
+}
+
+//name of the fucntion gives a hint 2 the reader about the expected return value
+//we can do that, when functions have a single expression in their body
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "sunday"
 
 fun  shouldChangeWater2 (
     temperature: Int = 22,
