443c410272b98225c82a3ea78bb6726ea0b422d3
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-02-27 18:04:00 +0100

    Random day

--- src/AWS.kt
+++ src/AWS.kt
@@ -1,8 +1,18 @@
+import java.util.*
+
 fun main(args: Array<String>) {
     println("Hello, ${args[0]}")
+    feedTheFish()
 
-    val isUnit = println("This is an expression")
-    println(isUnit)
+}
 
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
 
+fun randomDay() : String {
+    val week = listOf ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week [Random().nextInt(7)]  // 0 - 6
 }
