d1a6fd63ba187cc95eadf63b791c0b2d83dad69c
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-03-01 16:44:42 +0100

    overriding volume getter

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -6,6 +6,12 @@ class Aquarium{
     var height: Int = 40
     var length: Int = 100
 
+    //overriding getter
+    val  volume : Int
+        get(){
+            return width * height * length / 1000
+        }
+
     //one-liner declaration
-    fun volume() = width * height * length / 1000
+//    fun volume() = width * height * length / 1000
 }
