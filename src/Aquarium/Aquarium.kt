320bb74527ee202e601deebed52188203742851f
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-03-01 16:33:21 +0100

    Update Aquarium.kt
    Changed variable types to "var", so that they can be changed later.

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -2,7 +2,7 @@ package Aquarium
 
 class Aquarium{
 
-    val width: Int = 20
-    val height: Int = 40
-    val length: Int = 100
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
 }
