6f18aef564e90de97fc205d3437d9fc870e0e0b6
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2020-03-01 16:48:46 +0100

    Update Aquarium.kt
    Overrididng setter

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -7,11 +7,19 @@ class Aquarium{
     var length: Int = 100
 
     //overriding getter
-    val  volume : Int
-        get(){
-            return width * height * length / 1000
-        }
+//    val  volume : Int
+//        get(){
+//            return width * height * length / 1000
+//        }
 
     //one-liner declaration
 //    fun volume() = width * height * length / 1000
+
+    //overriding getter and setter
+    var  volume : Int
+        get(){
+            return width * height * length / 1000
+        }
+        // we could set it to private
+        set(value) { height = (value * 1000) / (width * length)
 }
