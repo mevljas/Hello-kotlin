36bebc0640c33670ffa71767ffbab55bc6e9930a
mevlja's <sm9299@student.uni-lj.si>
2020-08-05 16:51:34 +0200

    Add SpiceContainer

--- src/Aquarium/Decorations/Decorations.kt
+++ src/Aquarium/Decorations/Decorations.kt
@@ -1,5 +1,8 @@
 package Aquarium.Decorations
 
+import Spices.Curry
+import Spices.Spice
+
 fun main (args: Array<String>) {
     makeDecorations()
 }
@@ -44,3 +47,14 @@ data class Decorations(val rocks: String) {}
 data class Decorations2(val rocks: String, val wood: String, val diver: String){
 
 }
+
+data class SpiceContainer(var spice: Spice) {
+    val label = spice.name
+}
+
+
+val spiceCabinet = listOf(SpiceContainer(Curry("Yellow Curry", "mild")),
+    SpiceContainer(Curry("Red Curry", "medium")),
+    SpiceContainer(Curry("Green Curry", "spicy")))
+
+//for(element in spiceCabinet) println(element.label)
